3b3a9ea76927991ae61e0fd11341d1000352af5d
github username <crishanks12@gmail.com>
2019-03-19 14:15:40 -0700

    through form

--- src/index.js
+++ src/index.js
@@ -1,2 +1,91 @@
 // It might be a good idea to add event listener to make sure this file 
 // only runs after the DOM has finshed loading.
+document.addEventListener('DOMContentLoaded', () => {
+  getQuotes();
+  handleNewQuote();
+})
+
+const quotesURL = "http://localhost:3000/quotes";
+
+function getQuotes() {
+  fetch(quotesURL)
+  .then((results) => {
+    return results.json();
+  })
+  .then((json) => {
+    createQuotes(json);
+  })
+}
+
+
+function createQuotes(data) {
+  const ul = document.getElementById('quote-list');
+  data.forEach((quote) => {
+    //Create Elements
+    const li = document.createElement('li');
+    li.setAttribute('class', 'blockquote');
+    const blockquote = document.createElement('blockquote');
+    const p = document.createElement('p');
+    p.setAttribute('class', 'mb-0');
+    p.textContent = quote.quote;
+    const footer = document.createElement('footer');
+    footer.setAttribute('class', 'blockquote-footer');
+    footer.textContent = quote.author;
+    const br = document.createElement('br');
+    const likeButton = document.createElement('button');
+    likeButton.setAttribute('class', 'btn-success');
+    likeButton.textContent = "Likes: "
+    const span = document.createElement('span');
+    span.textContent = quote.likes;
+    const deleteButton = document.createElement('button');
+    deleteButton.setAttribute('class', 'btn-danger');
+    deleteButton.textContent = 'Delete';
+
+    //Append Elements
+    ul.appendChild(li);
+    li.appendChild(blockquote);
+    blockquote.appendChild(p);
+    blockquote.appendChild(footer);
+    blockquote.appendChild(br);
+    blockquote.appendChild(likeButton);
+    likeButton.appendChild(span);
+    blockquote.appendChild(deleteButton);
+  });
+}
+
+function addQuote() {
+  console.log('clicked')
+  const newQuote = document.getElementById('new-quote').value;
+  const newAuthor = document.getElementById('author').value;
+  const body = {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      "quote": `${newQuote}`,
+      "likes": 0,
+      "author": `${newAuthor}`
+    })
+  };
+
+  fetch(quotesURL, body)
+  .then(results => {
+    return results.json()
+  })
+  .then(json => {
+    console.log('json', json)
+    jsonArray = [json];
+    createQuotes(jsonArray);
+  })
+}
+
+function handleNewQuote() {
+  const form = document.getElementById('new-quote-form');
+  form.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+    addQuote();
+  });
+}
+
